7f6e74e94214cfc2861f0f4ffb5fa422477212c9
jason reid <jasonbrandon9406@gmail.com>
2018-09-13 23:02:08 +0100

    removed forward slash from url

--- compass.js
+++ compass.js
@@ -6,7 +6,9 @@ function initCompass(){
 	let compass = document.getElementById('compass-icon'),
 		speed = document.getElementById('speed'),
 		errorBox = document.querySelector('.error-heading'),
-		geolocationStatus = document.querySelector('.green');
+		geolocationStatus = document.querySelector('.green'),
+		listjson = document.querySelector('.geolocationdata');
+
 
 	if (navigator.geolocation) {
 		geolocationStatus.textContent = 'geo location browser';
@@ -14,8 +16,16 @@ function initCompass(){
 	
 		navigator.geolocation.watchPosition(function(data){
 			console.log(data);
+			let listitem = document.createElement('li');
+		
 			speed.textContent = data.coords.speed + "mph";
 			compass.style.transform = "rotate("+data.coords.heading+"deg)";
+			listitem.textContent = data.coords.heading;
+
+			console.log(typeof data);
+			
+
+			listjson.appendChild(listitem);
 			
 		}, function(err){
 			// errorBox.textContent = 'LOCATION PERMISSION DENIED. COMPASS NOT GOING TO WORK';

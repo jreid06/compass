5880bb6cb6e272680d79755d9002773262d40fce
jason reid <jasonbrandon9406@gmail.com>
2018-09-13 22:47:38 +0100

    first commit

---
+++ compass.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+
+function initCompass(){
+	let compass = document.getElementById('compass-icon'),
+		speed = document.getElementById('speed'),
+		errorBox = document.querySelector('.error-heading'),
+		geolocationStatus = document.querySelector('.green');
+
+	if (navigator.geolocation) {
+		geolocationStatus.textContent = 'geo location browser';
+		// navigator.geolocation.getCurrentPosition(moveIconCoords);
+	
+		navigator.geolocation.watchPosition(function(data){
+			console.log(data);
+			speed.textContent = data.coords.speed + "mph";
+			compass.style.transform = "rotate("+data.coords.heading+"deg)";
+			
+		}, function(err){
+			// errorBox.textContent = 'LOCATION PERMISSION DENIED. COMPASS NOT GOING TO WORK';
+		});
+
+		
+	}else{
+		console.log('not a geo location browser');
+		
+	}
+}
+
+
+document.getElementById('location').addEventListener('click', initCompass);
+// initCompass();
+
